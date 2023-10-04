81553881bee6cad119f5f3dca7e61472548daf06
Nikhil Sharma <nikhilsharma1574@gmail.com>
2023-10-04 20:17:06 +0530

    key error solving

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar = () => {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               {
                 LINKS.map((item, idx) => (
-                  <div className=''>
+                  <div className='' key={idx}>
                   <Link href={item.link} key={idx} className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#f6a339] md:p-0 dark:text-white md:dark:hover:text-[#f6a339] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>
                       {item.label}
                   </Link>
